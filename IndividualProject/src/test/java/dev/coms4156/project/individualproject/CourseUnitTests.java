118c84faa307ead5fefd77fd1834654ce8bf58a2
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-09-13 18:51:28 -0400

    test: create tests for the Course class and resolved bugs found

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,6 +1,8 @@
 package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -26,6 +28,57 @@ public class CourseUnitTests {
   public void toStringTest() {
     String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
     assertEquals(expectedResult, testCourse.toString());
+  }
+
+  @Test
+  public void enrollStudentTest() {
+    testCourse.setEnrolledStudentCount(100);
+    boolean enrollSuccess = testCourse.enrollStudent();
+    assertTrue(enrollSuccess, "Enrollment should succeed when capacity is not full.");
+  }
+
+  @Test
+  public void dropsStudentTest() {
+    testCourse.setEnrolledStudentCount(100);
+    boolean dropSuccess = testCourse.dropStudent();
+    assertTrue(dropSuccess, "Dropping should succeed when there's at least one student enrolled.");
+  }
+
+  @Test
+  public void dropStudentWhenNoneEnrolledTest() {
+    testCourse.setEnrolledStudentCount(0);
+    boolean dropSuccess = testCourse.dropStudent();
+    assertFalse(dropSuccess, "Dropping should fail with zero enrolled students.");
+  }
+
+  @Test
+  public void getCourseLocationTest() {
+    String expectedResult = "417 IAB";
+    assertEquals(expectedResult, testCourse.getCourseLocation());
+  }
+
+  @Test
+  public void getInstructorNameTest() {
+    String expectedResult = "Griffin Newbold";
+    assertEquals(expectedResult, testCourse.getInstructorName());
+  }
+
+  @Test
+  public void getCourseTimeSlotTest() {
+    String expectedResult = "11:40-12:55";
+    assertEquals(expectedResult, testCourse.getCourseTimeSlot());
+  }
+
+  @Test
+  public void isCourseFullTest() {
+    testCourse.setEnrolledStudentCount(250);
+    assertTrue(testCourse.isCourseFull(), "Course should be full.");
+  }
+
+  @Test
+  public void isCourseNotFullTest() {
+    testCourse.setEnrolledStudentCount(100);
+    assertFalse(testCourse.isCourseFull(), "Course should not be full.");
   }
 
   /** The test course instance used for testing. */
